e965ef08e38640fdc5b4b3c98a837587ee6165c2
annkostet <annakostetska30@gmail.com>
2021-11-17 20:05:11 +0200

    Add project files.

---
+++ Array Console/Program.cs
@@ -0,0 +1,31 @@
+﻿using ArrayLibrary;
+using System;
+
+namespace ArrayConsole
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[10];
+            Random random = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(100);
+            }
+            Console.WriteLine(Arrays.GetMaxOfArray(array));
+
+            //Array.Sort(array);
+            for (int i = 1; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+            Console.WriteLine();
+
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    Console.WriteLine($"{i} {Arrays.GetMinOfArray(array)}");
+            //}
+        }
+    }
+}
---
+++ ArrayLibrary/Arrays.cs
@@ -0,0 +1,107 @@
+﻿using System;
+
+namespace ArrayLibrary
+{
+    public class Arrays
+    {
+        public static int GetMinOfArray(int[] array)
+        {
+            int minValue = array[0];
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] < minValue)
+                {
+                    minValue = array[i];
+                }
+            }
+
+            return minValue;
+        }
+
+        public static int GetMaxOfArray(int[] array)
+        {
+            int maxValue = array[0];
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] > maxValue)
+                {
+                    maxValue = array[i];
+                }
+            }
+
+            return maxValue;
+        }
+
+        public static int GetIndexOfMinValueOfArray(int[] array)
+        {
+            int minValue = array[0];
+            int minIndex = 0;
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] < minValue)
+                {
+                    minValue = array[i];
+                    minIndex = i;
+                }
+            }
+
+            return minIndex;
+        }
+
+        public static int GetIndexOfMaxValueOfArray(int[] array)
+        {
+            int maxValue = array[0];
+            int maxIndex = 0;
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] > maxValue)
+                {
+                    maxValue = array[i];
+                    maxIndex = i;
+                }
+            }
+
+            return maxIndex;
+        }
+
+        public static int GetSumOfElementsWithOddIndexes(int[] array)
+        {
+            int sum = 0;
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    sum = sum + array[i];
+                }
+            }
+
+            return sum;
+        }
+
+        //public static int GetReverseOfArray(int[] array)
+        //{
+        //    int i = array.Length;
+        //    for (int j = 1; j < array.Length; j++)
+        //    {
+        //        int temp = array[j];
+        //        array[j] = array[i - j - 1];
+        //        array[i - j - 1] = temp;
+        //    }
+        //    return array[i];
+        //}
+
+        public static int GetCountOfOddElements(int[] array)
+        {
+            int count = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] % 2 != 0)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
